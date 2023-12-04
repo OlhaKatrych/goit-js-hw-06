f0d5bbfdd6e1849f4c695c3b91304ad3bac89644
OlhaKatrych <olgak198931@gmail.com>
2023-12-04 15:36:44 +0200

    task2

---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+class Storage {
+    #items;
+    constructor(items){
+        this.#items = items;
+    }
+    getItems(){
+        return this.#items;
+    }
+    addItem(newItem){
+        return this.#items.push(newItem);
+    }
+    removeItem(itemToRemove){
+        const res = this.#items.indexOf(itemToRemove);
+        return this.#items.splice(1, res);
+       
+    }
+
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
